8eea0ce4416a227a0a73b4cbda31fdac77e4b780
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2024-02-29 17:04:01 -0800

    Added pagefind astro integration

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,10 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()],
+  build: {
+    format: "file",
+  },
+  integrations: [tailwind(), mdx(), react(), pagefind()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
