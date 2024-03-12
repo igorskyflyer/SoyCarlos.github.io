9c46fa27d8111164573f674485ed17a6acb884c2
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2024-03-11 23:48:26 -0700

    Adding sitemap and robots.txt

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,17 +5,20 @@ import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 import pagefind from "astro-pagefind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   build: {
-    format: "file",
+    format: "file"
   },
-  integrations: [tailwind(), mdx(), react(), pagefind()],
+  integrations: [tailwind(), mdx(), react(), pagefind(), sitemap()],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
   renderers: ["@astrojs/renderer-react"],
+  site: 'https://www.carlos.soy/',
 });
